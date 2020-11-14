79a8fd5c27aaa8ac26e635fb7c490325f394b6f9
Alan Tan <alanwuha91@gmail.com>
2020-11-14 21:03:48 +0800

    feat: add mappings for constant and expression and add multiple method params

--- sample/CarMapper.java
+++ sample/CarMapper.java
@@ -4,9 +4,12 @@ public interface CarMapper {
     CarMapper INSTANCE = Mappers.getMapper( CarMapper.class );
 
     @Mapping(source = "numberOfSeats", target = "seatCount")
-    CarDto carToCarDto(Car car);
+    CarDto carToCarDto(Car car, Color color);
 
     @Mapping(source = "seatCount", target = "numberOfSeats")
+    @Mapping(source = "owner.name", target = "ownerName")
+    @Mapping(constant = CarBrandEnum.AUDI, target = "carBrand")
+    @Mapping(expression = "new Date()", target = "date")
     Car carDtoToCar(CarDto carDto);
 
 }
