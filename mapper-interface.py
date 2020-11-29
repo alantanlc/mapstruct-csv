6c5a5e674907e3dd6eb4739f80fef2e4847e9294
Alan Tan <alanwuha91@gmail.com>
2020-11-29 10:59:33 +0800

    feat: implement generate method to write mappings to file

--- mapper-interface.py
+++ mapper-interface.py
@@ -1,3 +1,4 @@
+import argparse
 import pandas as pd
 
 class Node:
@@ -16,11 +17,12 @@ class MapperInterface:
         self.filename = None
         self.lines = []
         self.root = None
+        self.mappings = []
 
     def load(self, filename, sheet):
         """Store texts from filename in self.lines as a list of lines"""
         with pd.ExcelFile(filename) as xlsx:
-            df = pd.read_excel(xlsx, sheet)
+            df = pd.read_excel(xlsx, sheet, na_filter='')
             self.lines = [(x[1], x[2], x[3]) for x in df.values[5:]]
         return self
 
@@ -36,6 +38,9 @@ class MapperInterface:
             depth = self.get_depth_from_value(line[0])
             node = self.get_last_node_at_depth(self.root, depth - 1)
             node.add(Node('.'.join([node.input, line[0].strip()]), line[2]))
+            if line[2]:
+                self.mappings.append(('.'.join([node.input, line[0].strip()]), line[2]))
+        return self
 
     def get_depth_from_value(self, value):
         """Return the node depth based on number of spaces in the beginning of value.
@@ -52,7 +57,19 @@ class MapperInterface:
             curr += 1
         return node
 
+    def generate(self):
+        with open('/'.join(['.', args.output, 'mappings.csv']), 'w') as f:
+            for mapping in self.mappings:
+                f.write(','.join(mapping))
+                f.write('\n')
+
 if __name__ == '__main__':
+    # argparse
+    parser = argparse.ArgumentParser(description='Parses a mapping excel and generates a list of mappings in CSV')
+    parser.add_argument('-f', '--filename', type=str, default='./sample/mapping.xlsx', help='name of mapping excel file')
+    parser.add_argument('-s', '--sheet', type=str, default='Sheet 2', help='name of sheet to parse in excel file')
+    parser.add_argument('-o', '--output', type=str, default='output', help='name of output directory')
+    args = parser.parse_args()
+
     m = MapperInterface()
-    m.load('./sample/message.xlsx', 'Mapping').parse()
-    print('hello')
+    m.load(args.filename, args.sheet).parse().generate()
